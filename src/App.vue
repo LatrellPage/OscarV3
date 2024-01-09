8a5db3fdd07a384070c9cb5b13e177c3167fbaf6
Latrell Page <pagelatrell14@gmail.com>
2024-01-09 04:59:05 -0500

    Initalized vue.js, added videos and black/white logogs to assets, and added video hero to the page

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <video 
+    :style="{
+      position: 'fixed',
+      top: '0',
+      left: '0',
+      width: '100vw',
+      height: '100vh',
+      objectFit: 'cover'
+    }"
+
+    autoplay
+    muted
+    loop
+    src="../Assets/videos/gamer-using-lighted-up-mouse-and-keyboard-playing-game-online-free-video.mp4"></video>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  components: {
+
+  }
+}
+</script>
+
+<style>
+body {
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+}
+</style>
