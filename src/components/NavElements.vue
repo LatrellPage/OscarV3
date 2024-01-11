d12032bfe23e0f4b05905d7b17567f0e365a2c46
Latrell Page <pagelatrell14@gmail.com>
2024-01-11 05:15:45 -0500

    Added header ad to home page and started on NavElements component

---
+++ src/components/NavElements.vue
@@ -0,0 +1,37 @@
+<template>
+    <div :class="nav-container">
+        <div :class="nav-element">Pc</div>
+        <div :class="nav-element">Accessories</div>
+        <div :class="nav-element">Discover</div>
+        <div :class="nav-element">Shop</div>
+    </div>
+</template>
+
+<script>
+    export default {
+	
+	data() {
+		return {
+
+		};
+	},
+	methods: {
+		
+	},
+	
+};
+</script>
+
+<style>
+.nav-container{
+    width: '50%';
+    display: 'flex';
+    justify-content: 'space-between';
+}
+
+.nav-element{
+    width: auto;
+    margin: auto;
+    color: white;
+}
+</style>
--- src/views/LandingPage.vue
+++ src/views/LandingPage.vue
@@ -1,4 +1,18 @@
 <template>
+	<div :class="headerAd">
+		<h1 :style="{ width: 'auto', color: 'white', fontWeight: 400 }">
+			Access over $5000 in credit and enjoy fast delivery by 01/01/2025
+			&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a
+				:style="{ fontWeight: '600', color: 'white' , textDecoration: 'underline', cursor: 'pointer'}"
+				>See Details</a
+			>
+		</h1>
+	</div>
+	<div :class="landing_page_navbar">
+		<svg></svg>
+		<NavElements/>
+		
+	</div>
 	<video
 		:style="{
 			position: 'fixed',
@@ -16,16 +30,56 @@
 </template>
 
 <script>
+import NavElements from '@/components/NavElements.vue';
+
+
 export default {
-	name: "App",
-	components: {},
+
+	data() {
+		return {
+			componenets: {
+				NavElements
+			},
+			headerAd: "header-ad",
+		};
+	},
 };
 </script>
 
-<style>
+<style scoped>
+
 body {
 	margin: 0;
 	padding: 0;
 	overflow: hidden;
 }
+
+h1 {
+	margin: 0;
+	padding: 0;
+}
+
+.header-ad {
+	width: 100%;
+	position: absolute;
+	top: 0;
+	left: 0;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	color: white;
+	z-index: 999;
+	font-size: 0.6rem;
+	background: linear-gradient(to right, #370e3c, #000000);
+	background-repeat: no-repeat;
+	height: 2.5rem;
+}
+
+.landing_page_navbar{
+	position: absolute;
+	left: 0;
+	display: flex;
+
+}
+
 </style>
